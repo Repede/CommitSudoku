a987ea1826083434495d70f256d31ba0cbc61d65
DrivePotato <ak89230@10.196.122.203>
2015-02-09 16:40:32 -0500

    Backtract v.0000000000001

--- SudokuResolver/src/Sudoku.java
+++ SudokuResolver/src/Sudoku.java
@@ -4,7 +4,33 @@ public class Sudoku
 	public static void main(String[] args)
 	{
 		// TODO SUICIDE BY SUDOKU
-		ArrayFactory fact = new ArrayFactory();
-		fact.getSudokuArrayFromFile("sudoku1.sud");
+		Sudoku sudoku = new Sudoku();
+		//sudoku.resolve();
 	}
+	
+//	public void resolve(){
+//		// Retrieve the sudoku grid.
+//		ArrayFactory fact = new ArrayFactory();
+//		this.grid = fact.getSudokuArrayFromFile("sudoku1.sud");
+//	}
+//
+//	private boolean backTrack(int row, int column){
+//		//try each number until it's valid
+//		for(int number=0; number<9; number++){
+//			if(isValid(this.grid, row, column, number)){
+//				
+//				this.grid[row][column] = number;
+//				int nextRow = this.retrieveNextRow();
+//				int nextColumn = this.retriveNextColumn(row);
+//				
+//				if(backTrack(nextRow, nextColumn)){
+//					return true;
+//				}
+//				
+//				this.grid[row][column] = 0;
+//			}
+//		}
+//		
+//		return false;
+//	}
 }
