b9ded0c24a2829cd721810998d04e65479cb08e0
am05280 <am05280@10.196.122.202>
2015-02-09 16:17:44 -0500

    Arrangement de la structure du code de la méthode SudokuLogic

--- SudokuResolver/src/SudokuLogic.java
+++ SudokuResolver/src/SudokuLogic.java
@@ -1,23 +1,32 @@
 
 public class SudokuLogic {
 
-
-	public boolean IsValide(int[][] tab,int starRow,int starColumm,int num)
+	public boolean isValid (int[][] tab, int startRow, int startColumm, int number)
 	{
-		//Checking box 3X3
-		int initPosX=starRow-starRow%3;
-		int initPosY=starColumm-starColumm%3;
-	    for(int i=initPosX;i<initPosX+3;i++)
-	    	for(int j=initPosY;i<initPosY+3;j++)
-	    		if(tab[i][j]==num)
-	    			return true;
-	    for(int i=0;i<8;i++)
-	    	if(tab[i][starColumm]==num)
-	    		return true;
-	    for(int j=0;j<8;j++)
-	    	if(tab[starRow][j]==num)
-	    		return true;
-	    return false;
+		//Regarde le carré
+		int initPosX = startRow - startRow % 3;
+		int initPosY = startColumm - startColumm % 3;
+	    for(int i = initPosX; i < initPosX + 3; i++) {
+	    	for(int j = initPosY; i < initPosY + 3; j++) {
+	    		if(tab[i][j] == number) {
+	    			return false;
+	    		}
+	    	}
+	    }
+	    //Regarde la ligne
+	    for(int i = 0; i < 8; i++) {
+	    	if(tab[i][startColumm] == number) {
+	    		return false;
+	    	}
+	    }
+    	//Regarde la colonne
+	    for(int j = 0; j < 8; j++) {
+	    	if(tab[startRow][j] == number) {
+	    		return false;
+	    	}
+	    }
+	    
+	    return true;
 	}
 	
 }
